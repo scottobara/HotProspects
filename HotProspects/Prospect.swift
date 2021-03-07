c3c3f35c08c821df45f5f1a346c4a2786103eae6
Scott Obara <scott@rubbscreations.com>
2021-03-07 11:28:28 +1100

    Day 82 of #100DaysOfSwiftUI: Project 16, part 4

---
+++ HotProspects/Prospect.swift
@@ -0,0 +1,23 @@
+//
+//  Prospect.swift
+//  HotProspects
+//
+//  Created by Scott Obara on 7/3/21.
+//
+
+import SwiftUI
+
+class Prospect: Identifiable, Codable {
+    var id = UUID()
+    var name = "Anonymous"
+    var emailAddress = ""
+    var isContacted = false
+}
+
+class Prospects: ObservableObject {
+    @Published var people: [Prospect]
+
+    init() {
+        self.people = []
+    }
+}
